7e4d7969beec422d7a1bcda636de9f7b5bc886f2
kuchenrolle <kuchenrolle@gmail.com>
2017-04-04 20:23:00 +0200

    add help text

--- src/redditquery/parse.py
+++ src/redditquery/parse.py
@@ -2,13 +2,36 @@
 import os
 import argparse
 
+
+description = """
+redditquery's behaviour can be changed with various parameters. Specifying mode is obligatory:
+
+mode:  1 Build Inverted Index (requires specifying -f and -l)
+       2 Query existing Inverted Index
+       3 Build Inverted Index and Query (requires specifying -f and -l)
+
+If the index is build, you will be required to specify the range of months to build the index on, by specifying the first and last month to be processed:
+
+-f --first: first month to be downloaded as YYYY/MM
+-l --last:  last month to be downloaded as YYYY/MM
+
+All other parameters are optional, here is what they do and their defaults:
+
+-d or --dir:      directory path to store data in (defaults to working dir)
+-c or --cores:    number of cores to use for downloading/decompressing monthly data (defaults to single-core)
+-m or --minfreq:  minimum frequeny to keep terms in index (defaults to 5)
+-n or --num:      number of results to show for each query (defaults to 10)
+-p or --progress: output progress information for download/processing (only single core, defaults to no progress shown)
+- h or --help:    show help file
+"""
+
 def parser():
     """Parses arguments from comman line using argparse.
     Parameters"""
     # default directory for reddit files
     default_directory = os.path.join(os.cwd(), "data")
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description = description)
     # obligatory
     parser.add_argument("mode", type = int, help = "execution mode: 1 build index, 2: query using existing index, 3 build and query")
     # conditionally obligatory
